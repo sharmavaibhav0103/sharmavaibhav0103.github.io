08ba7accf8054c6860da9e5f054b31a262bc75fc
sharmavaibhav0103 <61970310+sharmavaibhav0103@users.noreply.github.com>
2020-07-24 08:55:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 
 //declaring constants
+const mode = document.querySelector('.fa-adjust');
 const form = document.querySelector('.todo-form');
 const todo = document.querySelector('.todo-text');
+const submitBtn = document.querySelector('.btn-submit');
 const list = document.querySelector('.list-container');
 
+//Mode changer
+mode.addEventListener('click',function(){
+     const cr = getComputedStyle(document.documentElement)
+     .getPropertyValue('--bgColor');
+   
+     if(cr.trim() === 'black')
+        changeToWhite();
+     else if(cr.trim() === 'white')     
+        changeToBlack();    
+})
+
+function changeToWhite(){
+    document.documentElement.style.setProperty('--formColor', '#f3f3f4');
+    document.documentElement.style.setProperty('--bgColor', 'white');
+    document.documentElement.style.setProperty('--submitBtnColor', '#ffa351ff');
+    document.documentElement.style.setProperty('--listColor', '#ffa351ff');
+    document.documentElement.style.setProperty('--colorWhite', 'black');
+    document.documentElement.style.setProperty('--submitBtnTextColor', 'black');
+
+    const is = document.querySelectorAll('.fa-check, .fa-trash');
+    if(is.length > 0) {
+    is[0].style.backgroundColor = 'lightgreen';
+    is[0].style.color = 'black';
+    is[1].style.backgroundColor = 'red';
+    is[1].style.color = 'black';}
+}
+
+function changeToBlack(){
+    document.documentElement.style.setProperty('--bgColor', 'black');
+    document.documentElement.style.setProperty('--formColor', '#191919');
+    document.documentElement.style.setProperty('--submitBtnColor', '#333333');
+    document.documentElement.style.setProperty('--listColor', '#2d283e');
+    document.documentElement.style.setProperty('--colorWhite', 'white');
+    document.documentElement.style.setProperty('--submitBtnTextColor', 'rgb(170, 165, 165)');
+
+    const is = document.querySelectorAll('.fa-check, .fa-trash');
+    if(is.length > 0){
+    is[0].style.backgroundColor = '#2d283e';
+    is[0].style.color = 'gray';
+    is[1].style.backgroundColor = '#2d283e';
+    is[1].style.color = 'gray';}
+}
+
+//Adding a note
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let p = document.createElement('p');
